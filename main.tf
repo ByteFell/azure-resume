334353db353d458a50266bef0851b70ab9815e81
dev-DJF <djamesfell95@gmail.com>
2024-05-09 21:29:21 +0100

    started learning how to use terraform

---
+++ main.tf
@@ -0,0 +1,27 @@
+
+# This block provides config details for Terraform
+terraform{
+    required_providers {
+      azurerm = {
+        source = "hashicorp/azurerm"
+        version = "~>2.31.1"
+      }
+    }
+}
+
+# Provides config details for Azure terraform provider / I think he means ARM
+provider "azurerm" {
+  features{}
+}
+
+resource "azurerm_resource_group" "rg"{
+    name = "azure-resume-rg"
+    location = "ukwest"
+    tags = {
+        environment = "dev"
+        source = "Terraform"
+    }
+}
+resource "cosmosdb_account" "name" {
+  
+}
