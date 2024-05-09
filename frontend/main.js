0ff2e2b143c8a9701a66658d1c906865d2b90e8b
ByteFell <djamesfell95@gmail.com>
2024-05-09 13:02:59 +0100

    updates to main.js file

--- frontend/main.js
+++ frontend/main.js
@@ -1,16 +1,19 @@
+
+// winow object used for browser interaction
+// addEventListener'DOMContentLoaded' listens for html page loading
 window.addEventListener('DOMContentLoaded', (event)=>{
-    getVisitCount();
+    getVisitCount(); // calls getVisitCount after webpage loads
 })
 
 
 //This sets up the API variable with an empty string for now
-const funtionApi = '';
+const functionApi = ''; //declares the variable functionAPI - this will take in and store th api url
 
-//
-const getVisitCount = () =>{
-    let count = 30;
-    fetch(functionApi).then(Response =>{
-        return Response.json()
+// declares getVisitCount function 
+const getVisitCount = () =>{ // arrow function used to create readable block of code
+    let count = 30; //eclares initial count
+    fetch(functionApi).then(Response =>{ // fetches the api url from where its kept
+        return Response.json() // .then stores it as JSON for easier handling.
     }).then(Response =>{
         console.log("Website called function API.");
         count = Response.count;
