b73afeddcf6af685ba9a67b943a8b1d0ef0334ef
dev-DJF <djamesfell95@gmail.com>
2024-05-14 21:58:25 +0100

    Formatted js, updated main md

--- frontend/main.js
+++ frontend/main.js
@@ -3,7 +3,7 @@
 // addEventListener'DOMContentLoaded' listens for html page loading
 window.addEventListener('DOMContentLoaded', (event)=>{
     getVisitCount(); // calls getVisitCount after webpage loads
-})
+});
 
 
 //This sets up the API variable with an empty string for now
@@ -14,10 +14,12 @@ const getVisitCount = () =>{ // arrow function used to create readable block of 
     let count = 30; //eclares initial count
     fetch(functionApi).then(Response =>{ // fetches the api url from where its kept
         return Response.json() // .then stores it as JSON for easier handling.
+    
     }).then(Response =>{
         console.log("Website called function API."); // checking if the website calls api function
         count = Response.count; // sets joins api response with count
         document.getElementById("Counter").innerText = count; // add the visiter count to the html where ID is "counter"
+   
     }).catch(function(error){ // basic error handling
         console.log(error);
     });
